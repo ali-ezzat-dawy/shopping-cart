9345032dbe4f9c3ac4c77b357133cc74b79f6b41
AliEzzzat <aliezzatbegovich@gmail.com>
2022-11-14 16:37:40 +0200

    deploy app on gh-pages

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Store from "./Components/Store";
+import About from "./Components/About";
+import ShoppingCartProvider from "./Context/ShoppingCartContext";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <ShoppingCartProvider>
+          <Navbar />
+          <Container className="mb-4">
+            <Routes>
+              <Route path="/" element={<Store />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </Container>
+        </ShoppingCartProvider>
+      </Router>
+    </>
+  );
+};
+
+export default App;
